0d99907604eb90afef7a6a6240fe673290f87bca
Jose Everton <josefilho@ads.fiponline.edu.br>
2025-12-07 20:49:17 -0300

    Botão home corrigido

--- src/components/NavBar/Search_bar.tsx
+++ src/components/NavBar/Search_bar.tsx
@@ -8,7 +8,7 @@ import DropdownVaga from './DropdownVaga'
 export default function Bar() {
     return (
         <nav className="bg-transparent fixed w-full z-50 py-4 px-8 flex justify-end items-center">
-        <div className="flex space-x-6 bg-gray-800 bg-opacity-50 p-4 rounded-lg shadow-lg">
+        <div className="flex justify-between gap-6 bg-gray-800 bg-opacity-50 p-4 rounded-lg shadow-lg">
             <Link href="/dashboard" className="text-white hover:text-yellow-300 transition-colors">Home</Link>
             <Link href="/candidatoCadastro" className="text-white hover:text-yellow-300 transition-colors">Candidato</Link>
             <DropdownVaga />
